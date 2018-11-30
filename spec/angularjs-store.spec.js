784ab72580929528089ba17de6da3aaed6adf3fd
Rannie <rannieperalta092796@gmail.com>
2018-11-30 23:18:35 +0800

    Stub browser environment to use angular for test using benv
    * benv dependency jsdom must be on version 10

--- spec/angularjs-store.spec.js
+++ spec/angularjs-store.spec.js
@@ -1,8 +1,23 @@
 import Store from 'src/angularjs-store';
 import {expect} from 'chai';
+import benv from 'benv';
+
+beforeEach(function(done) {
+  benv.setup(function() {
+    benv.expose({
+      angular: benv.require('../../node_modules/angular/angular.js', 'angular')
+    });
+
+    done();
+  });
+});
 
 describe('AngularJS Store', () => {
   it('should be a function', () => {
     expect(Store).to.be.a('function');
   });
+});
+
+afterEach(function() {
+  benv.teardown();
 });
