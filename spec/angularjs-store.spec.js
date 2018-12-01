0a9fe3b1d842b663f01db3850baae1bf4a20472f
Rannie <rannieperalta092796@gmail.com>
2018-12-01 11:44:35 +0800

    Implement NgStore constructor
    * Change class name to NgStore
    * Add test for NgStore initializing

--- spec/angularjs-store.spec.js
+++ spec/angularjs-store.spec.js
@@ -1,9 +1,9 @@
-import Store from 'src/angularjs-store';
+import NgStore from 'src/angularjs-store';
 import {expect} from 'chai';
 import benv from 'benv';
 
-beforeEach(function(done) {
-  benv.setup(function() {
+before(function(done) {
+  benv.setup(function() {    
     benv.expose({
       angular: benv.require('../../node_modules/angular/angular.js', 'angular')
     });
@@ -13,11 +13,54 @@ beforeEach(function(done) {
 });
 
 describe('AngularJS Store', () => {
-  it('should be a function', () => {
-    expect(Store).to.be.a('function');
+
+  context('Constructor', () => {
+    it('should be a function', () => {
+      expect(NgStore).to.be.a('function');
+    });
+
+    it('should only accept object for initial state', () => {
+      expect(() => { new NgStore('foo') }).to.throw();
+      expect(() => { new NgStore(1) }).to.throw();
+      expect(() => { new NgStore(null) }).to.throw();
+      expect(() => { new NgStore(undefined) }).to.throw();
+      expect(() => { new NgStore(function () {}) }).to.throw();
+      expect(() => { new NgStore({}) }).to.not.throw();
+    });
+
+    it('should only accept function for custom copier', () => {
+      expect(() => { new NgStore({}, 'foo') }).to.throw();
+      expect(() => { new NgStore({}, 1) }).to.throw();
+      expect(() => { new NgStore({}, true) }).to.throw();
+      expect(() => { new NgStore({}, false) }).to.not.throw();
+      expect(() => { new NgStore({}, null) }).to.not.throw();
+      expect(() => { new NgStore({}, undefined) }).to.not.throw();
+      expect(() => { new NgStore({}, function () {}) }).to.not.throw();
+    });
+
+    it('should instance have a constructor name "NgStore"', () => {
+      const store = new NgStore({});
+      
+      expect(store.constructor.name).to.equal('NgStore');
+    });
+  });
+
+  // TODO
+  context('Method - copy', () => {
+    it('TODO', () => {});
+  });
+
+  // TODO
+  context('Method - dispatch', () => {
+    it('TODO', () => {});
+  });
+
+  // TODO
+  context('Method - hook', () => {
+    it('TODO', () => {});
   });
 });
 
-afterEach(function() {
+after(function() {
   benv.teardown();
 });
--- src/angularjs-store.js
+++ src/angularjs-store.js
@@ -1,9 +1,47 @@
-const Store = (function () {
-  function Store() {
-    this.state = [];
+const NgStore = (function () {
+  const $state = Symbol('state');
+
+  class NgStore {
+    /**
+     * @constructor
+     * @param {object} initialState - Initial state of store.
+     * @param {function} copier - Optional. A custom copier for store.
+     * @returns {NgStore} - NgStore instance.
+     */
+    constructor(initialState, copier) {
+      if (!angular.isObject(initialState)) {
+        throw new Error('Initial State must be an object');
+      }
+
+      this[$state] = initialState;
+
+      if (copier && !angular.isFunction(copier)) {
+        throw new Error('Copier must be a function');
+      }
+
+      this.copier = copier || angular.copy;
+    }
+
+    /**
+     * Get a new copy of current state.
+     * @todo Implement soon.
+     */
+    copy() {}
+
+    /**
+     * Dispatch an action for updating state.
+     * @todo Implement soon.
+     */
+    dispatch() {}
+
+    /**
+     * Listen for dispatched action in store to get the latest state.
+     * @todo Implement soon.
+     */
+    hook() {}
   }
 
-  return Store;
+  return NgStore;
 })();
 
-export default Store;
+export default NgStore;
