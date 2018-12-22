2c423770956cdc2487d0ea604e06aeebaa4da726
Rannie <rannieperalta092796@gmail.com>
2018-12-22 11:12:20 +0800

    Remove linter error from demo

--- demo/stores/group-a.store.js
+++ demo/stores/group-a.store.js
@@ -2,12 +2,10 @@ import NgStore from 'lib/angularjs-store';
 
 function GroupAStore() {
   const store = new NgStore({
-    stateA: {name: 'A', value: 'Apple'},
-    stateB: {name: 'B', value: 'Ball'},
-    stateC: {name: 'C', value: 'Cat'},
+    stateA: { name: 'A', value: 'Apple' },
+    stateB: { name: 'B', value: 'Ball' },
+    stateC: { name: 'C', value: 'Cat' },
   });
-
-  console.log(store.constructor.name)
 
   return store;
 }
